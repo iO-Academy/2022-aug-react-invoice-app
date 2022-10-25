4fbf1d3033314f012114aa07f62f30959b0cc689
owen <may.owend@gmail.com>
2022-10-25 13:02:07 +0100

    folder architecture modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Invoices from './components/Invoices';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
