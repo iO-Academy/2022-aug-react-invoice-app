b10d022accdecabd7bf6246ca7f96c08284fcd82
phone <pn330huboo@gmail.com>
2022-10-25 09:23:02 +0100

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Invoices from './components/Invoices';
 
 function App() {
--- src/components/Invoices/index.js
+++ src/components/Invoices/index.js
@@ -5,6 +5,17 @@ import ViewInvoice from "./ViewInvoice";
 const Invoices = () => {
     return (
         <>
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
             <InvoiceCards />
             <NewInvoice />
             <ViewInvoice />
