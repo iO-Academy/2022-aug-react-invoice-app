302d115b2343bdf2b06570a78dafb86b3320b1fd
phone <pn330huboo@gmail.com>
2022-10-26 10:12:49 +0100

    added # infront of invoice ref, formatted Due Date, dried up code to render status

--- src/components/Invoices/InvoiceCards/index.js
+++ src/components/Invoices/InvoiceCards/index.js
@@ -2,34 +2,42 @@ const InvoiceCards = ({invoice}) => {
     const { due, id, invoice_id, name, invoice_total, status, status_name } = invoice;
 
     const changeStatusColour = (status) => {
-        if (status == 1) {
-            return (
-                <ul className="ps-4 btn btn-outline-success m-0 disabled opacity-100">
-                    <li><strong>{status_name}</strong></li>
-                </ul>
-            )
-        } else if (status == 2) {
-            return (
-                <ul className="ps-4 btn btn-outline-warning m-0 disabled opacity-100">
-                    <li><strong>{status_name}</strong></li>
-                </ul>
-            )
-        } else {
-            return (
-                <ul className="ps-4 btn btn-outline-danger m-0 disabled opacity-100">
-                    <li><strong>{status_name}</strong></li>
-                </ul>
-            );
-        }
+        const statusMap = {
+            "1": "ps-4 btn btn-outline-success m-0 disabled opacity-100",
+            "2": "ps-4 btn btn-outline-warning m-0 disabled opacity-100",
+            "3": "ps-4 btn btn-outline-danger m-0 disabled opacity-100"
+        };
+        return (
+            <ul className={statusMap[status]}>
+                <li><strong>{status_name}</strong></li>
+            </ul>
+        )
+    }
+
+    const options = {
+        style: 'currency',
+        currency: 'GBP',
+    }
+    const invoicesTotalFloat = parseFloat(invoice_total);
+    const invoicesTotalFormatted = invoicesTotalFloat.toLocaleString("en-GB", options);
+
+    const dateFormatter = (dateString) => {
+        const date = new Date(dateString);
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        };
+        return (new Intl.DateTimeFormat('en-GB', options).format(date));
     }
 
     return (
         <>
             <div className="bg-white d-flex flex-wrap mb-2 justify-content-between align-items-center p-3 bd-highlight border rounded" key={id}>
-                    <div className="pe-4"><strong>{invoice_id}</strong></div>
-                    <div className="px-4 text-muted">Due {due}</div>
+                    <div className="pe-4">#<strong>{invoice_id}</strong></div>
+                    <div className="px-4 text-muted">Due {dateFormatter(due)}</div>
                     <div className="ps-4 text-muted">{name}</div>
-                    <div className="fs-4 mb-0 w-50 ps-md-4"><strong>£{invoice_total}</strong></div>
+                    <div className="fs-4 mb-0 w-50 ps-md-4"><strong>{invoicesTotalFormatted}</strong></div>
                     {changeStatusColour(status)}
                 <div className="p-0 d-none d-xl-block">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
