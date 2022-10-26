37505f9187921a5fccdeb0a48c8acec81b51cd8a
pedrofn <pedrofechado@gmail.com>
2022-10-26 15:54:21 +0100

    moved modal components to the top in the html flow

--- src/components/Invoices/index.js
+++ src/components/Invoices/index.js
@@ -22,7 +22,6 @@ const Invoices = () => {
     }
 
     // --- on mount logs the data retrieved ---
-    // --- Replace console.log(invoiceData) with setState inside the .then() to display data ---
     useEffect( () => {
         fetchInvoices()
             .then((invoiceData) => {
@@ -35,62 +34,61 @@ const Invoices = () => {
 
     return (
         <>
-            <div className="py-5 px-3">
-            <header className="d-flex justify-content-between align-items-end flex-wrap pt-3 px-0">
-                <div>
-                    <h1 className="fw-bolder">Invoices</h1>
-                    <p>There are 7 unpaid invoices</p>
-                </div>
-                <div className="d-flex justify-content-between py-3">
-                    <div className="dropdown">
-                        <button className="btn btn-light dropdown-toggle fs-6 fw-bold text-dark me-1" type="button"
-                                id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            Sort By
-                        </button>
-                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a className="dropdown-item fs-6" href="#">Invoice Reference</a></li>
-                            <li><a className="dropdown-item fs-6" href="#">Invoice Total</a></li>
-                            <li><a className="dropdown-item fs-6" href="#">Date Created</a></li>
-                            <li><a className="dropdown-item fs-6" href="#">Date Due</a></li>
-                        </ul>
-                    </div>
-                    <div className="dropdown">
-                        <button className="btn btn-light dropdown-toggle fs-6 fw-bold text-dark me-4" type="button"
-                                id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            Filter By Status
-                        </button>
-                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a className="dropdown-item fs-6" href="#">Paid</a></li>
-                            <li><a className="dropdown-item fs-6" href="#">Pending</a></li>
-                            <li><a className="dropdown-item fs-6" href="#">Cancelled</a></li>
-                            <li><a className="dropdown-item fs-6" href="#">Overdue</a></li>
-                            <li><a className="dropdown-item fs-6" href="#">View All</a></li>
-                        </ul>
-                    </div>
-                    <p>
-                        <button type="button" className="btn btn-info text-nowrap">
-                            <div className="bg-white d-inline block"><i className="bi bi-plus-square-fill"></i></div>
-                            <span className="text-white fs-5"> New Invoice</span>
-                        </button>
-                    </p>
-                </div>
-            </header>
-            <main>
-                {invoices.map((invoice) => {
-                        return (
-                            <InvoiceCards invoice={invoice} />
-                            );
-                    })
-                }
-
-            </main>
-            <footer>
-                <hr className="col-12 mt-4"/>
-                <div className="col-12">© Copyright iO Academy 2022</div>
-            </footer>
-            </div>
             <ViewInvoice />
             <NewInvoice />
+            <div className="py-5 px-3">
+                <header className="d-flex justify-content-between align-items-end flex-wrap pt-3 px-0">
+                    <div>
+                        <h1 className="fw-bolder">Invoices</h1>
+                        <p>There are 7 unpaid invoices</p>
+                    </div>
+                    <div className="d-flex justify-content-between py-3">
+                        <div className="dropdown">
+                            <button className="btn btn-light dropdown-toggle fs-6 fw-bold text-dark me-1" type="button"
+                                    id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Sort By
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a className="dropdown-item fs-6" href="#">Invoice Reference</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Invoice Total</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Date Created</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Date Due</a></li>
+                            </ul>
+                        </div>
+                        <div className="dropdown">
+                            <button className="btn btn-light dropdown-toggle fs-6 fw-bold text-dark me-4" type="button"
+                                    id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Filter By Status
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a className="dropdown-item fs-6" href="#">Paid</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Pending</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Cancelled</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Overdue</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">View All</a></li>
+                            </ul>
+                        </div>
+                        <p>
+                            <button type="button" className="btn btn-info text-nowrap" data-bs-toggle="modal" data-bs-target="#newInvoiceModal">
+                                <div className="bg-white d-inline block"><i className="bi bi-plus-square-fill"></i></div>
+                                <span className="text-white fs-5"> New Invoice</span>
+                            </button>
+                        </p>
+                    </div>
+                </header>
+                <main>
+                    {invoices.map((invoice) => {
+                            return (
+                                <InvoiceCards invoice={invoice} />
+                                );
+                        })
+                    }
+                </main>
+                <footer>
+                    <hr className="col-12 mt-4"/>
+                    <div className="col-12">© Copyright iO Academy 2022</div>
+                </footer>
+            </div>
         </>
     );
 }
