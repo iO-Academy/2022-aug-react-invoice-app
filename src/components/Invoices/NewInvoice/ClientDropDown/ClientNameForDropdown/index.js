f9a2a6ea06e7670ca6c8e94e29e5e2e34a7ecb1a
Carina <carina.volkes@gmail.com>
2022-10-27 10:49:13 +0100

    add client dropdown components

---
+++ src/components/Invoices/NewInvoice/ClientDropDown/ClientNameForDropdown/index.js
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react";
+import ClientDropDown from "../index";
+
+const ClientNameForDropdown = (props) => {
+    const {clientInfo, handleNameChange} = props;
+    return (
+        <>
+            <div>
+                <select onChange={handleNameChange} className="form-select" aria-label="Default select example">
+                    <option value="Select">Select from Client List</option>
+                    {clientInfo.map((clientInfo) =>
+                        <option key={clientInfo.id} value={clientInfo.id}>{clientInfo.name}</option>)}
+                </select>
+            </div>
+        </>
+    );
+}
+
+export default ClientDropDown;
--- src/components/Invoices/NewInvoice/ClientDropDown/index.js
+++ src/components/Invoices/NewInvoice/ClientDropDown/index.js
@@ -1,7 +1,42 @@
+import {useEffect, useState} from "react";
+import ClientNameForDropdown from "./ClientNameForDropdown";
+
 const ClientDropDown = () => {
+    const [clientsState, setClients] = useState({});
+
+    // --- fetch clients and returns json promise ---
+    const extractClientsResponseData = (response) => {
+        return response.json();
+    }
+
+    const fetchClientData = async () => {
+        const response = await fetch('http://localhost:8080/clients');
+
+        if (!response.ok) {
+            throw new Error('Data could not be fetched');
+        }
+
+        return await extractClientsResponseData(response);
+    }
+
+    useEffect(() => {
+        fetchClientData()
+            .then((clientData) => {
+                setClients(clientData);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, []);
+
+    // the following needs to be added to the modal and to update the client ID in post request
+    const handleNameChange = (e) => {
+        console.log(e.target.value);
+    }
+
     return (
         <>
-
+            {clientsState.data && <ClientNameForDropdown clientInfo={clientsState.data} handleNameChange={handleNameChange} />}
         </>
     );
 }
