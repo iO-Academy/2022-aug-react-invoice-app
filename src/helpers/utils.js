c69ecb49ffe4888a6cadd67a315e509f4e7b7886
Henry <henryppercy@gmail.com>
2022-10-28 13:10:14 +0100

    Changed cancelled colour within status colour function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,7 +2,7 @@ export const changeStatusColour = (status, status_name) => {
     const statusMap = {
         "1": "ps-4 btn btn-outline-success m-0 disabled opacity-100",
         "2": "ps-4 btn btn-outline-warning m-0 disabled opacity-100",
-        "3": "ps-4 btn btn-outline-danger m-0 disabled opacity-100"
+        "3": "ps-4 btn btn-outline-secondary m-0 disabled opacity-100"
     };
     return (
         <ul className={statusMap[status]}>
