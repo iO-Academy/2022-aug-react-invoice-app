7f9ddeab17b5f715421a20d67a2a7fc6d809bd67
richard <binghamrichard123@gmail.com>
2022-11-26 14:29:57 +0000

    got rows in new invoice mostly working and added client dropdown

--- src/components/Invoices/NewInvoice/index.js
+++ src/components/Invoices/NewInvoice/index.js
@@ -1,173 +1,186 @@
-// you will need to import dateFormatter via utils.js
+import {dateFormatter} from "../../../helpers/utils";
 import ClientDropDown from "./ClientDropDown";
-import NewInvoiceRows from "./NewInvoiceRows";
+
 import {useEffect, useState} from "react";
+import NewInvoiceRows from "./NewInvoiceRows";
 
-const initialRowState = [
-    {description: '', qty: 0, rate: 0, total: 0},
-];
-
-const NewInvoice = ({handleSubmit}) => {
-
-    const [rowsState, setRowsState] = useState(initialRowState);
+const NewInvoice = () => {
+    const [client, setClient] = useState(-1);
+    const [total, setTotal] = useState(0);
+    const [details, setDetails] = useState(
+        [{description: '', qty: 0, rate: 0, total: 0}]
+    );
 
     const handleCreateRow = (event) => {
-        event.preventDefault();
-        setRowsState([...rowsState, {description: '', qty: 0, rate: 0, total: 0}])
+        setDetails([...details, {description: '', qty: 0, rate: 0, total: 0}])
     }
 
     const handleDeleteRow = (index) => {
-        const newRowArray = rowsState.filter((element,idx) => {
+        const newDetails = details.filter((element, idx) => {
             return idx !== index;
         });
-        setRowsState(newRowArray);
+        setDetails(newDetails);
     }
 
-    const handleDescriptionChange = (index, value) => {
-        console.log('change ' + index);
-        rowsState[index].description = value;
-        console.log(rowsState);
+    const updateDetails = (rowNum, newDescription, newQuantity, newRate) => {
+        const newDetails = details.map((detailsRow, idx) => {
+            const {description, qty, rate, total} = detailsRow;
+            const newDetailsRow = {description, qty, rate, total};
+            if (idx === rowNum) {
+                newDetailsRow.description = newDescription ?? description;
+                newDetailsRow.qty = newQuantity ?? qty;
+                newDetailsRow.rate = newRate ?? rate;
+                newDetailsRow.total = newDetailsRow.qty * newDetailsRow.rate;
+            }
+            return newDetailsRow;
+        });
+        setDetails(newDetails);
     }
 
-    const todayDate = Date.now();
-    todayDate.toString();
-
-    const dateFormatter = (dateString) => { // this will be called from utils.js
-        const date = new Date(dateString);
-        const options = {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-        };
-        return (new Intl.DateTimeFormat('en-GB', options).format(date));
+    const handleDescriptionChange = (detailsRowNum, newDescription) => {
+        updateDetails(detailsRowNum, newDescription, null, null);
     }
 
+    const handleQuantityChange = (detailsRowNum, newQuantity) => {
+        updateDetails(detailsRowNum, null, newQuantity, null);
+    }
+
+    const handleRateChange = (detailsRowNum, newRate) => {
+        updateDetails(detailsRowNum, null, null, newRate);
+    }
+
+    useEffect(() => {
+        const newOverallTotal = details
+            .map((detailsRow) => detailsRow.total)
+            .reduce((overallTotal, rowTotal) => overallTotal + rowTotal);
+        setTotal(newOverallTotal);
+    }, [details]);
+
+    const todayDate = new Date();
     const dueDate = new Date();
-    dueDate.setDate(dueDate. getDate() + 30);
+    dueDate.setDate(todayDate.getDate() + 30);
 
     return (
-        <>
-            <div id="newInvoiceModal" className="modal container end-0" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title fw-bold">New Invoice</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close">
-                            </button>
-                        </div>
-                        <div className="modal-body overflow-auto px-3">
-                            <div className="container">
-                                <div className="row d-flex justify-content-between">
-                                    <div className="col-4">
-                                        <div>
-                                            <h6>From</h6>
-                                            <ul className="list-unstyled">
-                                                <li>Kermit the Frog</li>
-                                                <li>Kermit's Swamp</li>
-                                                <li>Beverly Hills</li>
-                                                <li>California</li>
-                                            </ul>
-                                        </div>
-                                        <div>
-                                            <h6>To</h6>
-                                            <ClientDropDown />
-                                        </div>
-                                    </div>
-                                    <div className="col-2"></div>
-                                    <div className="col-4">
-                                        <h6>Status</h6>
-                                        <ul className="btn btn-outline-warning ps-4 disabled opacity-100">
-                                            <li><strong>Pending</strong></li>
-                                        </ul>
-                                        <h6>Created</h6>
-                                        <p>{dateFormatter(todayDate)}</p>
-                                        <h6>Due</h6>
-                                        <p>{dateFormatter(dueDate)}</p>
-                                    </div>
+        <div id="newInvoiceModal" className="modal" tabIndex="-1">
+            <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">New Invoice</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close">
+                        </button>
+                    </div>
+
+                    <div className="modal-body px-3">
+                        <form className="container">
+                            <div className="d-flex justify-content-between">
+                                <div className="d-flex flex-column">
+                                    <h6><strong>From</strong></h6>
+                                    <ul className="list-unstyled">
+                                        <li>Kermit the Frog</li>
+                                        <li>Kermit's Swamp</li>
+                                        <li>Beverly Hills</li>
+                                        <li>California</li>
+                                    </ul>
+                                    <h6><strong>To</strong></h6>
+                                    {/*<ul className="list-unstyled">*/}
+                                    {/*    <li>Fiona</li>*/}
+                                        <ClientDropDown clientsState={client} setClients={setClient} />
+                                    {/*</ul>*/}
+                                </div>
+                                <div className="pe-5 d-flex flex-column">
+                                    <h6><strong>Status</strong></h6>
+                                    <ul className="btn btn-outline-warning ps-4">
+                                        <li><strong>Pending</strong></li>
+                                    </ul>
+                                    <h6><strong>Created</strong></h6>
+                                    <p>{dateFormatter(todayDate)}</p>
+                                    <h6><strong>Due</strong></h6>
+                                    <p>{dateFormatter(dueDate)}</p>
                                 </div>
                             </div>
-                            <table className="table">
-                                <thead className="border-top border-bottom">
-                                    <tr>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Quantity</th>
-                                        <th scope="col">Rate</th>
-                                        <th scope="col">Total</th>
-                                        <th scope="col"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                {rowsState.map((row, index) => {
-                                    return (
-                                        <tr className="mt-2">
-                                            <td>
-                                                <textarea
-                                                    className="form-control"
-                                                    rows="5"
-                                                    id="description"
-                                                    name="text"
-                                                    onChange={(event) => {
-                                                        handleDescriptionChange(event.target.value);
-                                                    }
-                                                    }
-                                                >
-                                                </textarea>
-                                            </td>
-                                            <td className="mb-3">
-                                                <input type="number" min="0" step="1" className="form-control" id="qty"/>
-                                            </td>
-                                            <td className="input mb-3" placeholder="0">
-                                                {/*<span className="input-group-text">£</span>*/}
-                                                <input type="number" min="0" step="1" className="form-control" id="rate"/>
-                                            </td>
-                                            <td>£</td>
-                                            <tr>
-                                                <td className="text-center">
-                                                    <button className="btn btn-success px-4 py-2 mb-3" type="submit" onClick={handleCreateRow}>+
-                                                    </button>
-                                                    <td>
-                                                        <button className="btn btn-danger px-4 py-2" type="submit" onClick={(event) => {
-                                                        event.preventDefault();
-                                                        handleDeleteRow(index);
-                                                    }}>-</button>
-                                                    </td>
-                                                </td>
-                                            </tr>
-                                        </tr>
-                                    );
-                                })}
-                                    {/*<NewInvoiceRows*/}
-                                    {/*    rowsState={rowsState}*/}
-                                    {/*    handleCreateRow={handleCreateRow}*/}
-                                    {/*    handleDeleteRow={handleDeleteRow}*/}
-                                    {/*    handleDescriptionChange={handleDescriptionChange}*/}
-                                    {/*/>*/}
-                                </tbody>
-                                <tfoot>
-                                    <tr className="bg-warning">
-                                        <td></td>
-                                        <td></td>
-                                        <td className="text-end fw-bold">Total</td>
-                                        <td></td>
-                                        <th className="text-end">£500.00</th>
-                                    </tr>
-                                </tfoot>
+                            <div className="row mt-2 gx-2 border-top border-bottom">
+                                <div className="col-4"><strong>Description</strong></div>
+                                <div className="col-2"><strong>Quantity</strong></div>
+                                <div className="col-2"><strong>Rate</strong></div>
+                                <div className="col-2"><strong>Total</strong></div>
+                                <div className="col-2"></div>
+                            </div>
 
-                            </table>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-success" data-bs-dismiss="modal"
-                            onClick={handleSubmit}>
-                                Create invoice
-                            </button>
-                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancel invoice</button>
-                        </div>
+                            {details.map((row, index) => {
+                                return (
+                                    <div className="row mt-2 gx-2">
+                                        <div className="col-4">
+                      <textarea
+                          className="form-control"
+                          rows="5"
+                          name="description"
+                          value={details[index].description}
+                          onChange={(ev) => handleDescriptionChange(index, ev.target.value)}
+                      >
+                      </textarea>
+                                        </div>
+                                        <div className="col-2">
+                                            <input
+                                                className="form-control"
+                                                type="number"
+                                                name="qty"
+                                                min="0"
+                                                step="1"
+                                                value={details[index].qty}
+                                                onChange={(ev) => handleQuantityChange(index, ev.target.value)}
+                                            />
+                                        </div>
+                                        <div className="col-2">
+                                            <div className="input-group">
+                                                <span className="input-group-text">£</span>
+                                                <input
+                                                    className="form-control"
+                                                    type="number"
+                                                    name="rate"
+                                                    min="0"
+                                                    step="1"
+                                                    value={details[index].rate}
+                                                    onChange={(ev) => handleRateChange(index, ev.target.value)}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="col-2">£{row.total}</div>
+                                        <div className="col-2 d-flex flex-column">
+                                            <button
+                                                className="btn btn-success py-2 mb-3"
+                                                type="button"
+                                                onClick={handleCreateRow}
+                                            >
+                                                +
+                                            </button>
+                                            <button
+                                                className="btn btn-danger py-2"
+                                                type="button"
+                                                onClick={() => handleDeleteRow(index)}
+                                            >
+                                                -
+                                            </button>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+
+                            <div className="row my-2 py-1 bg-warning text-end">
+                                <div className="col-10"><strong>Total</strong></div>
+                                <div className="col-2"><strong>£{total}</strong></div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-success">Create invoice</button>
+                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancel invoice</button>
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default NewInvoice;
