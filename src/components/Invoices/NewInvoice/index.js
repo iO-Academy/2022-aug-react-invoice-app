c0343e8fa5ab93a133441033e71c4fa60135a1a0
richard <binghamrichard123@gmail.com>
2023-01-15 08:39:01 +0000

    fixing merge issues

--- src/components/Invoices/NewInvoice/index.js
+++ src/components/Invoices/NewInvoice/index.js
@@ -1,12 +1,187 @@
+import {dateFormatter} from "../../../helpers/utils";
 import ClientDropDown from "./ClientDropDown";
-import InputTable from "./InputTable";
 
-const NewInvoice = () => {
+import {useEffect, useState} from "react";
+
+const NewInvoice = (props) => {
+    const [rowCountState, setRowCountState] = useState(0);
+
+    const {selectedClient, setSelectedClient, total, setTotal, details, setDetails, handleSubmit} = props;
+
+    const handleCreateRow = (event) => {
+        setRowCountState(rowCountState + 1)
+        setDetails([...details, {description: '', qty: 0, rate: 0, total: 0}])
+    }
+
+    const handleDeleteRow = (index) => {
+        if (rowCountState > 0) {
+            setRowCountState(rowCountState - 1);
+            const newDetails = details.filter((element, idx) => {
+                    return idx !== index;
+                }
+            );
+            setDetails(newDetails);
+        }
+    }
+
+    const updateDetails = (rowNum, newDescription, newQuantity, newRate) => {
+        const newDetails = details.map((detailsRow, idx) => {
+            const {description, qty, rate, total} = detailsRow;
+            const newDetailsRow = {description, qty, rate, total};
+            if (idx === rowNum) {
+                newDetailsRow.description = newDescription ?? description;
+                newDetailsRow.qty = newQuantity ?? qty;
+                newDetailsRow.rate = newRate ?? rate;
+                newDetailsRow.total = newDetailsRow.qty * newDetailsRow.rate;
+            }
+            return newDetailsRow;
+        });
+        setDetails(newDetails);
+    }
+
+    const handleDescriptionChange = (detailsRowNum, newDescription) => {
+        updateDetails(detailsRowNum, newDescription, null, null);
+    }
+
+    const handleQuantityChange = (detailsRowNum, newQuantity) => {
+        updateDetails(detailsRowNum, null, newQuantity, null);
+    }
+
+    const handleRateChange = (detailsRowNum, newRate) => {
+        updateDetails(detailsRowNum, null, null, newRate);
+    }
+
+    useEffect(() => {
+        const newOverallTotal = details
+            .map((detailsRow) => detailsRow.total)
+            .reduce((overallTotal, rowTotal) => overallTotal + rowTotal);
+        setTotal(newOverallTotal);
+    }, [details]);
+
+    const todayDate = new Date();
+    const dueDate = new Date();
+    dueDate.setDate(todayDate.getDate() + 30);
+
     return (
-        <>
-            <ClientDropDown />
-            <InputTable />
-        </>
+        <div id="newInvoiceModal" className="modal" tabIndex="-1">
+            <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">New Invoice</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close">
+                        </button>
+                    </div>
+
+                    <div className="modal-body px-3">
+                        <form className="container">
+                            <div className="d-flex justify-content-between">
+                                <div className="d-flex flex-column">
+                                    <h6><strong>From</strong></h6>
+                                    <ul className="list-unstyled">
+                                        <li>Kermit the Frog</li>
+                                        <li>Kermit's Swamp</li>
+                                        <li>Beverly Hills</li>
+                                        <li>California</li>
+                                    </ul>
+                                    <h6><strong>To</strong></h6>
+                                    <ClientDropDown
+                                        selectedClient={selectedClient}
+                                        setSelectedClient={setSelectedClient}
+                                    />
+                                </div>
+                                <div className="pe-5 d-flex flex-column">
+                                    <h6><strong>Status</strong></h6>
+                                    <ul className="btn btn-outline-warning ps-4">
+                                        <li><strong>Pending</strong></li>
+                                    </ul>
+                                    <h6><strong>Created</strong></h6>
+                                    <p>{dateFormatter(todayDate)}</p>
+                                    <h6><strong>Due</strong></h6>
+                                    <p>{dateFormatter(dueDate)}</p>
+                                </div>
+                            </div>
+                            <div className="row mt-2 gx-2 border-top border-bottom">
+                                <div className="col-4"><strong>Description</strong></div>
+                                <div className="col-2"><strong>Quantity</strong></div>
+                                <div className="col-2"><strong>Rate</strong></div>
+                                <div className="col-2"><strong>Total</strong></div>
+                                <div className="col-2"></div>
+                            </div>
+
+                            {details.map((row, index) => {
+                                return (
+                                    <div className="row mt-2 gx-2">
+                                        <div className="col-4">
+                      <textarea
+                          className="form-control"
+                          rows="5"
+                          name="description"
+                          value={details[index].description}
+                          onChange={(ev) => handleDescriptionChange(index, ev.target.value)}
+                      >
+                      </textarea>
+                                        </div>
+                                        <div className="col-2">
+                                            <input
+                                                className="form-control"
+                                                type="number"
+                                                name="qty"
+                                                min="0"
+                                                step="1"
+                                                value={details[index].qty}
+                                                onChange={(ev) => handleQuantityChange(index, ev.target.value)}
+                                            />
+                                        </div>
+                                        <div className="col-2">
+                                            <div className="input-group">
+                                                <span className="input-group-text">£</span>
+                                                <input
+                                                    className="form-control"
+                                                    type="number"
+                                                    name="rate"
+                                                    min="0"
+                                                    step="1"
+                                                    value={details[index].rate}
+                                                    onChange={(ev) => handleRateChange(index, ev.target.value)}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="col-2">£{row.total}</div>
+                                        <div className="col-2 d-flex flex-column">
+                                            <button
+                                                className="btn btn-success py-2 mb-3"
+                                                type="button"
+                                                onClick={handleCreateRow}
+                                            >
+                                                +
+                                            </button>
+                                            <button
+                                                className="btn btn-danger py-2"
+                                                type="button"
+                                                onClick={() => handleDeleteRow(index)}
+                                            >
+                                                -
+                                            </button>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+
+                            <div className="row my-2 py-1 bg-warning text-end">
+                                <div className="col-10"><strong>Total</strong></div>
+                                <div className="col-2"><strong>£{total}</strong></div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-success" data-bs-dismiss="modal" onClick={handleSubmit}>Create invoice</button>
+                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancel invoice</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/components/Invoices/index.js
+++ src/components/Invoices/index.js
@@ -1,11 +1,9 @@
 import InvoiceCards from "./InvoiceCards";
 import NewInvoice from "./NewInvoice";
 import ViewInvoice from "./ViewInvoice";
-import {useEffect, useState} from "react";
-import DropdownElements from "./DropDownElements";
+import {useCallback, useEffect, useState} from "react";
 
 const Invoices = () => {
-
     const detailedInvoice = {
         data: {
             id: "",
@@ -30,10 +28,13 @@ const Invoices = () => {
         }
     }
 
-    const initialDetailsState = [];
-
+    const [selectedClient, setSelectedClient] = useState(-1);
+    const [total, setTotal] = useState(0);
+    const [details, setDetails] = useState(
+        [{description: '', qty: 0, rate: 0, total: 0}]
+    );
     const [detailedInvoiceState, setDetailedInvoiceState] = useState(detailedInvoice);
-    const [detailsState, setDetailsState] = useState(initialDetailsState);
+    const [detailsState, setDetailsState] = useState([]);
 
     const fetchDetailedInvoice = async (invoiceId) => {
         const response = await fetch(`http://localhost:8080/invoices/${invoiceId}`);
@@ -50,7 +51,6 @@ const Invoices = () => {
     }
 
     const [invoices, setInvoices] = useState([]);
-
     // --- fetch invoices and returns json promise ---
     const extractResponseData = (response) => {
         return response.json();
@@ -64,7 +64,8 @@ const Invoices = () => {
         return await extractResponseData(response);
     }
 
-    useEffect( () => {
+    const updateInvoices = useCallback(() => {
+
         fetchInvoices()
             .then((invoiceData) => {
                 setInvoices(invoiceData.data);
@@ -72,51 +73,140 @@ const Invoices = () => {
             .catch((err) => {
                 err.message = 'Error! Could not resolve promise.';
             });
+
+    }, [invoices]);
+
+    // --- on mount logs the data retrieved ---
+    useEffect( () => {
+        updateInvoices()
     }, []);
 
     // --- filtered unpaid invoices ---
     const filteredInvoices = invoices.filter(invoice => invoice.status === "2");
     const unpaidInvoices = filteredInvoices.length;
 
+
+    // --- Send POST request to invoices database and returns json promise ---
+    const postNewInvoice = async (data) => {
+        const response = await fetch('http://localhost:8080/invoices',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        if (!response.ok) {
+            throw new Error();
+        }
+        updateInvoices();
+        return await extractResponseData(response);
+    }
+
+    // --- handles newInvoice submission ---
+    const handleSubmit = () => {
+        postNewInvoice({
+            "client": parseInt(selectedClient),
+            "total": total,
+            "details":
+                details.map((row) => {
+                    return (
+
+                        {
+                            "quantity": Number(row.qty),
+                            "rate": Number(row.rate),
+                            "total": row.total,
+                            "description": row.description
+                        }
+                    )
+                })
+
+        })
+            .then((res) => {
+                // --- data to update invoices State ---
+                const newInvoiceDetails = {
+                    id: res.data.id,
+                    invoice_id: res.data.invoice_id,
+                    name: "Holly-anne Crothers",
+                    due: "2021-12-30",
+                    invoice_total: "4485.93",
+                    status: "1",
+                    status_name: "Paid"
+                }
+                setInvoices((prevInvoiceState) => {
+                    return [...prevInvoiceState, newInvoiceDetails];
+                })
+            })
+            .catch((err) => {
+                err.message = 'Error! Could not post this new invoice.';
+            });
+    }
+
+
     return (
         <>
             <ViewInvoice detailedInvoiceState={detailedInvoiceState} detailsState={detailsState}/>
-
-            <div className="py-5 px-5">
-            <header className="d-flex justify-content-between align-items-end flex-wrap pt-3 px-0">
-                <div>
-                    <h1 className="fw-bolder">Invoices</h1>
-                    <p>There are {unpaidInvoices} unpaid invoices</p>
-                </div>
-                <div className="d-flex justify-content-between py-3">
-                    <DropdownElements dropdownName={'Sort By'}
-                                      listItemsArray={['Invoice Reference', 'Invoice Total', 'Date Created', 'Date Due']}/>
-                    <DropdownElements dropdownName={'Filter By Status'}
-                                      listItemsArray={['Paid', 'Pending', 'Cancelled', 'Overdue', 'View All']}/>
-                    <p>
-                        <button type="button" className="btn btn-info text-nowrap">
-                            <div className="badge text-bg-dark p-1"><i className="fa-solid fa-plus fa-lg"></i></div>
-                            <span className="text-white fs-5"> New Invoice</span>
-                        </button>
-                    </p>
-                </div>
-            </header>
-            <main>
-                {invoices.map((invoice) => {
+            <NewInvoice
+                selectedClient={selectedClient}
+                setSelectedClient={setSelectedClient}
+                total={total}
+                setTotal={setTotal}
+                details={details}
+                setDetails={setDetails}
+                handleSubmit={handleSubmit}
+            />
+            <div className="py-5 px-3">
+                <header className="d-flex justify-content-between align-items-end flex-wrap pt-3 px-0">
+                    <div>
+                        <h1 className="fw-bolder">Invoices</h1>
+                        <p>There are {unpaidInvoices} unpaid invoices</p>
+                    </div>
+                    <div className="d-flex justify-content-between py-3">
+                        <div className="dropdown">
+                            <button className="btn btn-light dropdown-toggle fs-6 fw-bold text-dark me-1" type="button"
+                                    id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Sort By
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a className="dropdown-item fs-6" href="#">Invoice Reference</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Invoice Total</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Date Created</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Date Due</a></li>
+                            </ul>
+                        </div>
+                        <div className="dropdown">
+                            <button className="btn btn-light dropdown-toggle fs-6 fw-bold text-dark me-4" type="button"
+                                    id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Filter By Status
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a className="dropdown-item fs-6" href="#">Paid</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Pending</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Cancelled</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">Overdue</a></li>
+                                <li><a className="dropdown-item fs-6" href="#">View All</a></li>
+                            </ul>
+                        </div>
+                        <p>
+                            <button type="button" className="btn btn-info text-nowrap" data-bs-toggle="modal" data-bs-target="#newInvoiceModal">
+                                <div className="badge text-bg-dark p-1"><i className="fa-solid fa-plus fa-lg"></i></div>
+                                <span className="text-white fs-5"> New Invoice</span>
+                            </button>
+                        </p>
+                    </div>
+                </header>
+                <main>
+                    {invoices.map((invoice) => {
                         return (
                             <InvoiceCards handleCardClick={handleCardClick} invoice={invoice} key={invoice.id}/>
-                            );
+                        );
                     })
-                }
-
-            </main>
-            <footer>
-                <hr className="col-12 mt-4"/>
-                <div className="col-12">© Copyright iO Academy 2022</div>
-            </footer>
+                    }
+                </main>
+                <footer>
+                    <hr className="col-12 mt-4"/>
+                    <div className="col-12">© Copyright iO Academy 2022</div>
+                </footer>
             </div>
-
-            <NewInvoice />
         </>
     );
 }
